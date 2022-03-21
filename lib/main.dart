58bdb5adda767c2680c7361a0ab439f038b7b2bc
Vishwajeet <jeet06071997.com>
2022-03-21 18:34:02 +0530

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'EducationDetails.dart';
 import 'drawer.dart';
 
 void main() => runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Home(),
     ));
 
@@ -31,122 +32,127 @@ class _HomeState extends State<Home> {
                 radius: 80.0,
               ),
             ),
-            SizedBox(height: 60.0),
-            Text(
-              'Vishwajeet Maurya ',
-              style: TextStyle(
-                color: Color.fromARGB(255, 18, 32, 231),
-                letterSpacing: 2.0,
-                fontWeight: FontWeight.bold,
-                fontFamily: 'DancingScript-VariableFont_wght',
-                fontSize: 35.0,
-              ),
-            ),
-            SizedBox(height: 30.0),
             Container(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  IconButton(
-                    onPressed: () {},
-                    icon: Icon(Icons.mail_outline_sharp),
-                    color: Color.fromARGB(255, 230, 33, 33),
-                  ),
-                  SizedBox(width: 20.0),
-                  Text(
-                    'vishwajeetm9@gmail.com',
-                    style: TextStyle(
-                      color: Color.fromARGB(255, 20, 20, 20),
-                      //letterSpacing: 2.0,
-
-                      fontSize: 25.0,
+                padding: EdgeInsets.all(20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    SizedBox(height: 60.0),
+                    Container(
+                      child: Text(
+                        'Vishwajeet Maurya ',
+                        style: TextStyle(
+                          color: Color.fromARGB(255, 18, 32, 231),
+                          letterSpacing: 2.0,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: 'DancingScript-VariableFont_wght',
+                          fontSize: 35.0,
+                        ),
+                      ),
                     ),
-                  ),
-                ],
-              ),
-            ),
-            SizedBox(height: 30.0),
-            Container(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  IconButton(
-                    onPressed: () {},
-                    icon: Icon(Icons.phone),
-                    color: Color.fromARGB(255, 115, 123, 233),
-                  ),
-                  SizedBox(width: 20.0),
-                  Text(
-                    '7007515832',
-                    style: TextStyle(
-                      color: Color.fromARGB(255, 12, 12, 12),
-                      //letterSpacing: 2.0,
+                    SizedBox(height: 20.0),
+                    Container(
+                      child: Row(
+                        children: [
+                          IconButton(
+                            onPressed: () {},
+                            icon: Icon(Icons.mail_outline_sharp),
+                            color: Color.fromARGB(255, 230, 33, 33),
+                          ),
+                          SizedBox(width: 20.0),
+                          Text(
+                            'vishwajeetm9@gmail.com',
+                            style: TextStyle(
+                              color: Color.fromARGB(255, 20, 20, 20),
+                              //letterSpacing: 2.0,
 
-                      fontSize: 25.0,
+                              fontSize: 25.0,
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                ],
-              ),
-            ),
-            SizedBox(height: 30.0),
-            Container(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  IconButton(
-                    onPressed: () {},
-                    icon: Icon(Icons.work_outlined),
-                    color: Color.fromARGB(255, 104, 99, 99),
-                  ),
-                  SizedBox(width: 20.0),
-                  Text(
-                    'Flutter UI Developer',
-                    style: TextStyle(
-                      color: Color.fromARGB(255, 19, 18, 18),
-                      //letterSpacing: 2.0,
+                    SizedBox(height: 20.0),
+                    Container(
+                      child: Row(
+                        children: [
+                          IconButton(
+                            onPressed: () {},
+                            icon: Icon(Icons.phone),
+                            color: Color.fromARGB(255, 115, 123, 233),
+                          ),
+                          SizedBox(width: 20.0),
+                          Text(
+                            '70XXXXX2',
+                            style: TextStyle(
+                              color: Color.fromARGB(255, 12, 12, 12),
+                              //letterSpacing: 2.0,
 
-                      fontSize: 25.0,
+                              fontSize: 25.0,
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  IconButton(
-                    onPressed: () {},
-                    icon: Icon(Icons.location_on),
-                    color: Colors.redAccent,
-                  ),
-                  SizedBox(width: 20.0),
-                  Text(
-                    '251/1 Omnagar, Sultanpur',
-                    style: TextStyle(
-                      color: Color.fromARGB(255, 20, 20, 20),
-                      //letterSpacing: 2.0,
+                    SizedBox(height: 20.0),
+                    Container(
+                      child: Row(
+                        children: [
+                          IconButton(
+                            onPressed: () {},
+                            icon: Icon(Icons.work_outlined),
+                            color: Color.fromARGB(255, 104, 99, 99),
+                          ),
+                          SizedBox(width: 20.0),
+                          Text(
+                            'Flutter UI Developer',
+                            style: TextStyle(
+                              color: Color.fromARGB(255, 19, 18, 18),
+                              //letterSpacing: 2.0,
 
-                      fontSize: 25.0,
+                              fontSize: 25.0,
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                ],
-              ),
-            ),
-            SizedBox(height: 30.0),
-            RaisedButton.icon(
-              padding: EdgeInsets.fromLTRB(40, 10, 40, 10),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => EducationDetails()),
-                );
-              },
-              icon: Icon(Icons.school),
-              color: Colors.amber[400],
-              label: Text('EDUCATION DETAILS'),
-            ),
+                    Container(
+                      child: Row(
+                        children: [
+                          IconButton(
+                            onPressed: () {},
+                            icon: Icon(Icons.location_on),
+                            color: Colors.redAccent,
+                          ),
+                          SizedBox(width: 20.0),
+                          Text(
+                            '251/1 Omnagar,XYZ',
+                            style: TextStyle(
+                              color: Color.fromARGB(255, 20, 20, 20),
+                              //letterSpacing: 2.0,
+
+                              fontSize: 25.0,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 20.0),
+                    RaisedButton.icon(
+                      padding: EdgeInsets.fromLTRB(40, 10, 40, 10),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => EducationDetails()),
+                        );
+                      },
+                      icon: Icon(Icons.school),
+                      color: Colors.amber[400],
+                      label: Text('EDUCATION DETAILS'),
+                    ),
+                  ],
+                )),
           ],
         ),
       ),
